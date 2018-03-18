3f3075c4714842154deb5054bccc98b83e97fdbe
ViachaslauStrelnikau <damned.dementor@gmail.com>
2018-03-19 00:03:28 +0300

    Fix some errors

--- model/src/main/java/com/epam/brest/model/Department.java
+++ model/src/main/java/com/epam/brest/model/Department.java
@@ -1,8 +1,5 @@
 package com.epam.brest.model;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 /**
  * Department class.
  */
--- web-app/src/main/java/com/epam/brest/web_app/controllers/ErrorController.java
+++ web-app/src/main/java/com/epam/brest/web_app/controllers/ErrorController.java
@@ -4,16 +4,20 @@ import org.omg.CosNaming.NamingContextPackage.NotFound;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
 
 
 @ControllerAdvice
 public class ErrorController extends RuntimeException {
 
+    /**
+     * method handleException handle app exceptions
+     *
+     * @param model attributes map
+     * @param ex Exception
+     * @return template name
+     */
     @ExceptionHandler(Exception.class)
-    public String handleResourceNotFoundException( Exception  ex,Model model) {
+    public String handleException( Exception  ex,Model model) {
         model.addAttribute  ("Title","Exception occured");
         model.addAttribute  ("Text",ex.toString());
         return "exception";
