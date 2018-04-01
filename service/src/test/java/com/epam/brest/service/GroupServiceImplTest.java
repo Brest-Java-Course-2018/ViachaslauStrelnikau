fe5889c56edb6d228d9bf2a8d489f5cc8873f865
ViachaslauStrelnikau <damned.dementor@gmail.com>
2018-04-01 10:34:11 +0300

    Added student service tests

--- service/src/test/java/com/epam/brest/service/GroupServiceImplTest.java
+++ service/src/test/java/com/epam/brest/service/GroupServiceImplTest.java
@@ -2,6 +2,7 @@ package com.epam.brest.service;
 
 import com.epam.brest.dao.GroupDao;
 import com.epam.brest.dto.GroupDTO;
+import com.epam.brest.dto.GroupDTOlite;
 import com.epam.brest.model.Group;
 import org.easymock.EasyMock;
 import org.junit.After;
@@ -26,11 +27,28 @@ public class GroupServiceImplTest {
     @Autowired
     private GroupDao mockGroupDao;
 
+    private static Group group;
+    private static Group group2;
+
     private static GroupDTO groupDTO;
     private static GroupDTO groupDTO2;
 
+    private static GroupDTOlite groupDTOlite;
+    private static GroupDTOlite groupDTOlite2;
+
     @Before
     public void testSetUp() {
+        group = new Group();
+        group.setFullName("Test1");
+        group.setShortName("T1");
+        group.setDescription("Test");
+        group2 = new Group();
+        group2.setGroupId(1);
+        group2.setFullName("Test1");
+        group2.setShortName("T1");
+        group2.setDescription("Test");
+
+
         groupDTO = new GroupDTO();
         groupDTO.setGroupId(1);
         groupDTO.setFullName("Test1");
@@ -42,11 +60,19 @@ public class GroupServiceImplTest {
         groupDTO2.setFullName("Test2");
         groupDTO2.setGroupAvgMarks(2.2);
         groupDTO2.setShortName("T2");
+
+        groupDTOlite = new GroupDTOlite();
+        groupDTOlite.setGroupId(1);
+        groupDTOlite.setFullName("Test1");
+        groupDTOlite2 = new GroupDTOlite();
+        groupDTOlite2.setGroupId(2);
+        groupDTOlite2.setFullName("Test2");
     }
 
     @After
     public void TestClear() {
         EasyMock.verify(mockGroupDao);
+        EasyMock.reset(mockGroupDao);
     }
 
     @Test
@@ -58,4 +84,45 @@ public class GroupServiceImplTest {
 
     }
 
+    @Test
+    public void getallGroupsDTOliteTest() {
+        EasyMock.expect(mockGroupDao.getallGroupsDTOlite()).andReturn(Arrays.asList(groupDTOlite, groupDTOlite2));
+        EasyMock.replay(mockGroupDao);
+
+        groupService.getallGroupsDTOlite();
+
+    }
+
+    @Test
+    public void getGroupByIdTest() {
+        EasyMock.expect(mockGroupDao.getGroupById(1)).andReturn(group);
+        EasyMock.replay(mockGroupDao);
+
+        groupService.getGroupById(1);
+    }
+
+    @Test
+    public void addGroupTest() {
+        EasyMock.expect(mockGroupDao.addGroup(group)).andReturn(group2);
+        EasyMock.replay(mockGroupDao);
+
+        groupService.addGroup(group);
+
+    }
+
+    @Test
+    public void updateGroupTest() {
+        mockGroupDao.updateGroup(group);
+        EasyMock.replay(mockGroupDao);
+
+        groupService.updateGroup(group);
+    }
+
+    @Test
+    public void removeGroup() {
+        mockGroupDao.removeGroup(1);
+        EasyMock.replay(mockGroupDao);
+
+        groupService.removeGroup(1);
+    }
 }
