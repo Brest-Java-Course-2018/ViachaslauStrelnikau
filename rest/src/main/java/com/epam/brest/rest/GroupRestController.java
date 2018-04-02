10d0f7c27ab7da252a6fef6d4839c52e1249cda4
ViachaslauStrelnikau <damned.dementor@gmail.com>
2018-04-02 22:04:30 +0300

    Rest module started

---
+++ rest/src/main/java/com/epam/brest/rest/GroupRestController.java
@@ -0,0 +1,10 @@
+package com.epam.brest.rest;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GroupRestController {
+    private static final Logger LOGGER= LogManager.getLogger();
+}
