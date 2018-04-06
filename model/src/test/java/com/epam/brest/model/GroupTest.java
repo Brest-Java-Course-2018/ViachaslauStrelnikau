2a97e245db9e86c4eb30ac8a813a5a5e708c9a0d
ViachaslauStrelnikau <damned.dementor@gmail.com>
2018-04-06 11:50:31 +0300

    Student rest client tests added

---
+++ model/src/test/java/com/epam/brest/model/GroupTest.java
@@ -0,0 +1,48 @@
+package com.epam.brest.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Objects;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test Group class
+ */
+public class GroupTest {
+
+    private static int GROUPID =1;
+    private static String SHORTNAME = "test";
+    private static String FULLNAME = "test group";
+    private static String DESCRIPTION = "description";
+
+    @Test
+    public void grouptest()
+    {
+        Group group = new Group();
+        group.setGroupId(GROUPID);
+        group.setShortName(SHORTNAME);
+        group.setFullName(FULLNAME);
+        group.setDescription(DESCRIPTION);
+
+        Assert.assertEquals(group.getGroupId(),GROUPID);
+        Assert.assertEquals(group.getShortName(),SHORTNAME);
+        Assert.assertEquals(group.getFullName(),FULLNAME);
+        Assert.assertEquals(group.getDescription(),DESCRIPTION);
+        String toString= "Group{"
+                + "groupId=" + GROUPID
+                + ", shortName='" + SHORTNAME + '\''
+                + ", fullName='" + FULLNAME + '\''
+                + ", description=" + DESCRIPTION
+                + '}';
+        Assert.assertEquals(group.toString(),toString);
+
+        Group group1 = new Group(SHORTNAME,FULLNAME,DESCRIPTION);
+        group1.setGroupId(GROUPID);
+
+        Assert.assertEquals(group,group1);
+        Assert.assertEquals(group1.hashCode(), Objects.hash(GROUPID,SHORTNAME,FULLNAME,DESCRIPTION));
+    }
+
+}
