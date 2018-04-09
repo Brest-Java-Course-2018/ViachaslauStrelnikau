cb0a726bf2816f0210e6348ed5dc51bc9bf77e9f
ViachaslauStrelnikau <damned.dementor@gmail.com>
2018-04-09 15:53:46 +0300

    web-app groupedit.html added

--- web-app/src/main/java/com/epam/brest/web_app/controllers/GroupController.java
+++ web-app/src/main/java/com/epam/brest/web_app/controllers/GroupController.java
@@ -1,6 +1,7 @@
 package com.epam.brest.web_app.controllers;
 
 import com.epam.brest.dto.GroupDTO;
+import com.epam.brest.model.Group;
 import com.epam.brest.service.GroupService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Collection;
 
@@ -22,10 +24,20 @@ public class GroupController {
     @GetMapping(value = "/groups")
     public String groups (final Model model)
     {
-        LOGGER.debug("GroupController groups");
+        LOGGER.debug("GroupController - groups");
         Collection<GroupDTO> groupDTOS= groupService.getallGroupsDTO();
         model.addAttribute("groups",groupDTOS);
         return "groups";
+    }
+
+    @GetMapping(value = "/groups/{id}")
+    public String editGroup(@PathVariable(value = "id") Integer id, Model model)
+    {
+        LOGGER.debug("GroupController - editGroup:{}",id);
+        Group group = groupService.getGroupById(id);
+        model.addAttribute("isNew",false);
+        model.addAttribute("group",group);
+        return "editgroups";
     }
 
 
