8e44a17ee1a3657bda6256b8395058721289f6f1
ViachaslauStrelnikau <damned.dementor@gmail.com>
2018-04-09 16:59:59 +0300

    web-app edit group added

--- web-app/src/main/java/com/epam/brest/web_app/controllers/GroupController.java
+++ web-app/src/main/java/com/epam/brest/web_app/controllers/GroupController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 
@@ -39,6 +40,11 @@ public class GroupController {
         model.addAttribute("group",group);
         return "editgroups";
     }
-
-
+    @PostMapping(value = "/groups/{id}")
+    public String updateGroup(Group group)
+    {
+        LOGGER.debug("GroupController - updateGroup:{}",group);
+        groupService.updateGroup(group);
+        return "redirect:/groups";
+    }
 }
