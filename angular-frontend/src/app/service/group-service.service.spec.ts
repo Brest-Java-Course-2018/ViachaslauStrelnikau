270a65b07da973f5ed0c539803171da33d08b380
Damned Dementor <damned.dementor@gmail.com>
2019-04-18 17:05:55 +0300

    Angular simple test added

--- angular-frontend/src/app/service/group-service.service.spec.ts
+++ angular-frontend/src/app/service/group-service.service.spec.ts
@@ -2,8 +2,11 @@ import {TestBed} from '@angular/core/testing';
 
 import {GroupServiceService} from './group-service.service';
 import {HttpClientModule} from "@angular/common/http";
+import {GroupDto} from "../model/group-dto";
 
 describe('GroupServiceService', () => {
+
+  const groups:GroupDto[]=[];
   beforeEach(() => TestBed.configureTestingModule({
 
     imports: [
@@ -16,5 +19,10 @@ describe('GroupServiceService', () => {
   it('should be created', () => {
     const service: GroupServiceService = TestBed.get(GroupServiceService);
     expect(service).toBeTruthy();
+  });
+
+  it('should use ValueService', () => {
+    const service: GroupServiceService = TestBed.get(GroupServiceService);
+    expect(typeof service.getGroups()).toBe(typeof groups);
   });
 });
